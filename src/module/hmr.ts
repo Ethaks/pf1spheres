c3fa41995bf8929002e42bc80b51ed25b008c02a
Ethaks <ethaks@protonmail.ch>
2022-05-12 16:01:42 +0200

    build: improve handlebars HMR by re-rendering opened applications

--- src/module/hmr.ts
+++ src/module/hmr.ts
@@ -22,5 +22,10 @@ if (import.meta.hot) {
     Handlebars.registerPartial(file, compiled);
     _templateCache[file] = compiled;
     console.log(`${MODULE_ID} | Compiled template ${file}`);
+
+    // Rerender opened applications to make use of updated templates
+    for (const appId in ui.windows) {
+      ui.windows[Number(appId)].render(true);
+    }
   });
 }
