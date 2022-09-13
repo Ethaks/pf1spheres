e3bf5b4865ea1bb0c166dc37e39811d2290420cd
Ethaks <ethaks@protonmail.ch>
2022-09-13 20:25:04 +0200

    chore: add license header to d20roll types

--- src/pf1-types/d20roll.d.ts
+++ src/pf1-types/d20roll.d.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Ethaks <ethaks@pm.me>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 export interface D20RollConstructorOptions extends RollTerm.EvaluationOptions {
   /**
    * The number on the d20 that is considered a critical success.
